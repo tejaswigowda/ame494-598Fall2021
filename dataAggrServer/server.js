e5b359a70bf148bf542ea1d5b131b96a8454b714
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-09-23 13:09:42 -0700

    ac

--- dataAggrServer/server.js
+++ dataAggrServer/server.js
@@ -6,13 +6,18 @@ var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 1234;
 
+var MS = require("mongoskin");
+var db = MS.db("mongodb://localhost:27017/sensorData")
+
 server.get("/", function (req, res) {
     res.redirect("index.html")
 });
 
 server.get("/sendData", function (req, res) {
   console.log(req.query);
-  res.end("1");
+  db.collection("data").insert(req.query, function(err, result){
+    res.end("1");
+  });
 });
 
 server.use(methodOverride());
