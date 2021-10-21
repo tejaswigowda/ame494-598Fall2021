37657c87378dc39b8c388c6e1ec96c6cd91cc8e2
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-10-21 13:10:42 -0700

    ac

--- ASSIGNMENTS/A5/code/ws-server/server.js
+++ ASSIGNMENTS/A5/code/ws-server/server.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");
 const app = express();
 const WebSocket = require('ws')
@@ -8,14 +6,17 @@ var MS = require("mongoskin");
 var db = MS.db("mongodb://localhost:27017/sensorData")
 
 
+
 const wss = new WebSocket.Server({ port: 3000})
 
 wss.on('connection', ws => {
   ws.on('message', message => {
-     console.log(`Received message => ${message}`)
-    db.collection("dataWS").insert(`${message}`, function(err, result){
-    });
+	var x = `${message}`;
+	  console.log(x)
+	  try{
+		    db.collection("dataWS").insert(JSON.parse(x), function(err, result){
+		    });
+	  }
+	  catch(e){}
   })
-  ws.send('start');
 })
-
